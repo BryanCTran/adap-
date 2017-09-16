0ba718108c07d4bb9c1c6c0dab04f133bc62fb3e
Austin Mathew <austomat57@gmail.com>
2017-09-16 01:18:00 -0400

    Notifications added for good and bad

--- mobile/src/main/java/com/adapplus/adap/MainActivity.java
+++ mobile/src/main/java/com/adapplus/adap/MainActivity.java
@@ -1,52 +1,18 @@
 package com.adapplus.adap;
 
 import android.app.Notification;
-import android.content.Intent;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
-
-    public Button sendgood;
-
-        public void init() {
-
-        sendgood= (Button)findViewById(R.id.sendgood);
-        sendgood.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-
-                //Intent notify = new Intent(sendNotification(View view);)
-                public void sendNotification(View view) {
-                                    Notification notification = new NotificationCompat.Builder(getApplication())
-                            .setSmallIcon(R.mipmap.ic_launcher)
-                            .setContentTitle("ADAP+")
-                            .setContentText("Good job!")
-                            .extend(new NotificationCompat.WearableExtender().setHintShowBackgroundOnly(true))
-                            .build();
-                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplication());
-                    int notificationId = 1;
-                    notificationManager.notify(notificationId, notification);
-                }
-            }
-        });
-
-
-
-
-
-    }
-
-
-
-
+//Firsttrybelow
+   // NotificationCompat.Builder goodjob;
+   // private static final int uniqueID = 456654;
 
 
 
@@ -55,8 +21,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText = (EditText) findViewById(R.id.editText);
+//Firsttry=previous method, look into this if change is needed https://www.youtube.com/watch?v=NgQzJ0s0XmM
+     //   goodjob = new NotificationCompat.Builder(this);
+       // goodjob.setAutoCancel(true);
+
     }
+
+  //  firsttry: public void goodjobnotify(View view){
+    //    goodjob.setSmallIcon(R.mipmap.ic_launcher);
+      //  goodjob.setTicker("Good Job!");
+
+
+    //}
+
+    public void goodjobnotify(View view) {
+        //Notification code for positive reinforcement
+        Notification notification = new NotificationCompat.Builder(getApplication())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("ADAP+")
+                .setContentText("Good Job!")
+                .extend(new NotificationCompat.WearableExtender().setHintShowBackgroundOnly(true))
+                .build();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplication());
+        int notificationId = 1;
+        notificationManager.notify(notificationId, notification);
+    }
+
+    public void badjobnotify(View view) {
+        //Notification code for negative reinforcement
+        Notification notification = new NotificationCompat.Builder(getApplication())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("ADAP+")
+                .setContentText("Don't do that!")
+                .extend(new NotificationCompat.WearableExtender().setHintShowBackgroundOnly(true))
+                .build();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplication());
+        int notificationId = 1;
+        notificationManager.notify(notificationId, notification);
+    }
+
     public void sendNotification(View view) {
+        //Notification code for custom text
         String toSend = editText.getText().toString();
         if(toSend.isEmpty())
             toSend = "You sent an empty notification";
