e85c197537869833cb03b7051ded41678abecc12
Austin Mathew <austomat57@gmail.com>
2017-12-08 22:10:03 -0500

    Created Settings Menu GUI Added Dropdown vibration options for both command types

--- mobile/src/main/java/com/adapplus/adap/Settings.java
+++ mobile/src/main/java/com/adapplus/adap/Settings.java
@@ -4,13 +4,40 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Settings extends AppCompatActivity {
+    Spinner viblist;
+    String intensitylist[] = {"Pattern 1","Pattern 2", "Pattern 3"};
+    ArrayAdapter<String> adapterviblist;
+
+    Spinner viblistneg;
+
+    ArrayAdapter<String> adapterviblistneg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        viblist = (Spinner) findViewById(R.id.spinner4);
+
+        adapterviblist = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, intensitylist);
+        adapterviblist.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        viblist.setAdapter(adapterviblist);
+
+
+        viblistneg = (Spinner) findViewById(R.id.spinner3);
+
+        adapterviblistneg = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, intensitylist);
+        adapterviblistneg.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        viblistneg.setAdapter(adapterviblistneg);
+
+
     }
 
     public void settomain(View view){
